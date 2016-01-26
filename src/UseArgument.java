497ad156cd86367e847160f00d97692cabbd6bd4
Marseglia. Mike <mike.marseglia@hasbro.com>
2016-01-25 19:21:51 -0500

    add apache ant build file

--- src/UseArgument.java
+++ src/UseArgument.java
@@ -1,5 +1,5 @@
 public class UseArgument {
-  public static void main(String[] args) {
+  public static void main (String[] args) {
     System.out.print("Hi, ");
     System.out.print(args[0]);
     System.out.println(" how are you?");
